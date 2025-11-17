5f391c15295baffc249630090b0f1b241eeb7ead
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-11-17 15:51:09 +0100

    Design the data insert

--- data.sql
+++ data.sql
@@ -17,18 +17,18 @@ INSERT INTO HealthCenters (HealthCenterName, Location)
 INSERT INTO Patients (PersonalNumber, FirstName, LastName, PhoneNumber, 
 Email, BirthDate, IsActive BOOLEAN DEFAULT TRUE, HealthCenterID)
 VALUES 
-("199356789542","Fatoom","Hussein","077777777","fatoom@gmail.com",1993 , TRUE,1),
-("198454321764","Ahmed","Abdulmalek","073333333","ahmed@gmail.com",1984 ,FALSE,2),
-("201449524252","Sukaina","Ahmed","075555555","sukaina@gmail.com",2014, FALSE ,1),
-("199652395686","Sara","Karlsson","079999999","sara@gmail.com",1996 ,3),
-("200047345271","Sara","Johansson","070707078","sara23@gmail.com",2000 ,4),
-("200178567492","Anna","Zucchini","076688668","anna@gmail.com",2001, TRUE,5),
-("202510103456","Santiago","Velasquez","076453623","santiago@gmail.com",2025,TRUE ,6),
-("199706236754","Natalia","Velasquez","078964328","natalia@gmail.com",1997, TRUE,6),
-("198311294381","Josephine","Fischhaber","0784623536","josephine@gmail.com",1983,TRUE ,7),
-("188812223879","Danial","Andersson","074863529","danial@gmail.com",1888, TRUE,8),
-("198206307643","Eloise","Metso","076666687","eloise@gmail.com",1982,TRUE ,8),
-("202203258726","Oskar","Longman","078473654","oskar@gmail.com",2022,TRUE ,9)
+("199356789542","Fatoom","Hussein","077777777","fatoom@gmail.com",1993 , TRUE,1),-- 1
+("198454321764","Ahmed","Abdulmalek","073333333","ahmed@gmail.com",1984 ,FALSE,2),-- 2
+("201449524252","Sukaina","Ahmed","075555555","sukaina@gmail.com",2014, FALSE ,1),-- 3
+("199652395686","Sara","Karlsson","079999999","sara@gmail.com",1996 ,3),-- 4
+("200047345271","Sara","Johansson","070707078","sara23@gmail.com",2000 ,4),-- 5
+("200178567492","Anna","Zucchini","076688668","anna@gmail.com",2001, TRUE,5),-- 6
+("202510103456","Santiago","Velasquez","076453623","santiago@gmail.com",2025,TRUE ,6),-- 7
+("199706236754","Natalia","Velasquez","078964328","natalia@gmail.com",1997, TRUE,6),-- 8
+("198311294381","Josephine","Fischhaber","0784623536","josephine@gmail.com",1983,TRUE ,7),-- 9
+("188812223879","Danial","Andersson","074863529","danial@gmail.com",1888, TRUE,8),-- 10
+("198206307643","Eloise","Metso","076666687","eloise@gmail.com",1982,TRUE ,8),-- 11
+("202203258726","Oskar","Longman","078473654","oskar@gmail.com",2022,TRUE ,9)-- 12
 ;
 
 INSERT INTO Roles (RoleName, CanAccessJournal BOOLEAN DEFAULT FALSE )
@@ -45,18 +45,18 @@ VALUES
 
 INSERT INTO Staff (FirstName, LastName, Email, RoleID , HealthCenterID)
 VALUES
-("Fatima","Al-Murtadha","fatima12@gmail.com",True,4 , 1),
-("Mathias","Jarlsson","mathias@gmail.com",True,8 , 2),
-("Mohammed","Ali","mohammed@gmail.com",True, 1 ,2 ),
-("Minshao","Kai","minsha@gmail.com",True, 1, 1),
-("Milton","Frozen","milton@gmail.com",True, 2,2 ),
-("Laith","Saleh","laith@gmail.com",True, 2, 1),
-("Sumaya","","sumaya@gmail.com",True, 3,1 ),
-("Jana","Sami","@gmail.com",True, 5, ),
-("Escada","Niabo","escada@gmail.com",True, 6, ),
-("Lukas","Björnberg","lukas@gmail.com",True, 7, ),
-("Safa","Said","safa@gmail.com",True, 8,5 ),
-("Thalia","Metso","thalia@gmail.com",True, 1, 3)
+("Fatima","Al-Murtadha","fatima12@gmail.com",True,4 , 1), -- 1
+("Mathias","Jarlsson","mathias@gmail.com",True,8 , 2),-- 2
+("Mohammed","Ali","mohammed@gmail.com",True, 1 ,2 ),-- 3
+("Minshao","Kai","minsha@gmail.com",True, 1, 1),-- 4
+("Milton","Frozen","milton@gmail.com",True, 2,2 ),-- 5
+("Laith","Saleh","laith@gmail.com",True, 2, 1),-- 6
+("Sumaya","","sumaya@gmail.com",True, 3,1 ),-- 7
+("Jana","Sami","@gmail.com",True, 5, ),-- 8
+("Escada","Niabo","escada@gmail.com",True, 6, ),-- 9
+("Lukas","Björnberg","lukas@gmail.com",True, 7, ),-- 10
+("Safa","Said","safa@gmail.com",True, 8,5 ),-- 11
+("Thalia","Metso","thalia@gmail.com",True, 1, 3) -- 12
 
 ;
 
@@ -64,33 +64,65 @@ VALUES
 
 INSERT INTO Bookings ( BookingDate, PatientID, Purpose, HealthCenterID )
 VALUES
-(DATE , 1,"Diaria",1 ), (DATE , ,"", ), (DATE , ,"", ), 
-(DATE , ,"", ), (DATE , ,"", ), (DATE , ,"", ), 
-(DATE , ,"", ), (DATE , ,"", ), (DATE , ,"", ), 
-(DATE , ,"", ), (DATE , ,"", ), (DATE , ,"", )
+(DATE , 1,"Diaria",1 ), -- 1
+(DATE ,2 ,"Andnings problem",2 ),-- 2
+ (DATE , 3,"Andnings problem", 1), -- 3
+(DATE , ,"Ont i mage", ),-- 4
+ (DATE , ,"Huvudvärk", ),-- 5
+  (DATE , ,"Graviditet", ), -- 6
+(DATE , ,"Prov Tagning", ), -- 7
+(DATE , ,"Förnya recept", ),-- 8
+ (DATE , ,"Hälsokontroll", ), -- 9
+(DATE , ,"Undersökning", ), -- 10
+(DATE , ,"Feber", ),-- 11
+ (DATE , ,"Hosta", )-- 12
 
 ;
 
-INSERT INTO Journals (JournalID, PatientID, StaffID, BookingID)
+INSERT INTO Journals ( PatientID, StaffID, BookingID)
 VALUES
-( , , , ),( , , , ),( , , , ),
-( , , , ),( , , , ),( , , , ),
-( , , , ),( , , , ),( , , , ),
-( , , , ),( , , , ),( , , , ),
+( , , ),-- 1
+( , ,  ),-- 2
+( , ,  ),-- 3
+(  , , ),-- 4
+( , ,  ),-- 5
+( , ,  ),-- 6
+(  , , ),-- 7
+( , ,  ),-- 8
+( , ,  ),-- 9
+( , ,  ),-- 10
+( , ,  ),-- 11
+( , ,  )-- 12
 ;
 
 INSERT INTO JournalDetails (JournalDetailID, NoteTitle, NoteDate, Note, NextProcedure, WriterID, JournalID)
 VALUES
-( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),
-( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),
-( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),
-( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),( ,"", DATE, "", "", ),
+( ,"", DATE, "", "", ),-- 1
+( ,"", DATE, "", "", ),-- 2
+( ,"", DATE, "", "", ),-- 3
+( ,"", DATE, "", "", ),-- 4
+( ,"", DATE, "", "", ),-- 5
+( ,"", DATE, "", "", ),-- 6
+( ,"", DATE, "", "", ),-- 7
+( ,"", DATE, "", "", ),-- 8
+( ,"", DATE, "", "", ),-- 9
+( ,"", DATE, "", "", ),-- 10
+( ,"", DATE, "", "", ),-- 11
+( ,"", DATE, "", "", )-- 12
 ;
 
 INSERT INTO BookingStaff (BookingID, StaffID, Role)
 VALUES
-( , , ""),( , , ""),( , , ""),
-( , , ""),( , , ""),( , , ""),
-( , , ""),( , , ""),( , , ""),
-( , , ""),( , , ""),( , , ""),
+( , , ""),-- 1
+( , , ""),-- 2
+( , , ""),-- 3
+( , , ""),-- 4
+( , , ""),-- 5
+( , , ""),-- 6
+( , , ""),-- 7
+( , , ""),-- 8
+( , , ""),-- 9
+( , , ""),-- 10
+( , , ""),-- 11
+( , , "")-- 12
 ;
